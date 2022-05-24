60d94e70029e94cc068896be529521579cef3467
Nathan Golightly <n8golightly@gmail.com>
2022-05-24 14:01:52 -0500

    frontend first renderings

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Allmart</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
