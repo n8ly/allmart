99382003063be259084388eaaef4c6670a6c98a1
Nathan Golightly <n8golightly@gmail.com>
2022-05-26 21:35:44 -0500

    header, main & footer bootstrapped

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/navbar';
+import Container from 'react-bootstrap/container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Allmart</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Allmart</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* <Link to="/">Allmart</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
