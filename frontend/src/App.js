f3d6d9d19e51b22b2ce431d913579e161c824210
Nathan Golightly <n8golightly@gmail.com>
2022-06-04 15:31:48 -0500

    sign-in screen ui implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -39,8 +40,9 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
-              <Route path="/" element={<HomeScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
         </main>
---
+++ frontend/src/screens/SigninScreen.js
@@ -0,0 +1,36 @@
+import { Helmet } from 'react-helmet-async';
+import { Link, useLocation } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export default function SigninScreen() {
+  const { search } = useLocation();
+  const redirectInURL = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInURL ? redirectInURL : '/';
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign In</title>
+      </Helmet>
+      <h1 className="my-3">Sign In</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" required />
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Sign In</Button>
+        </div>
+        <div className="mb-3">
+          New Customer?{' '}
+          <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
+        </div>
+      </Form>
+    </Container>
+  );
+}
