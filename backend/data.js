81a15564f36f45fb523d4fc525b35029ce5add5a
Nathan Golightly <n8golightly@gmail.com>
2022-06-06 16:12:03 -0500

    Create sample users: admin & customer

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Nathan',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       // _id: '1',
