cdcdea8987d5efb5003c4fdad9d584c6bec2712f
Nathan Golightly <n8golightly@gmail.com>
2022-11-04 13:46:21 -0500

    Update Readme with Overview Merge branch 'master' of https://github.com/EsserID/hr-den15-data-structures i Please enter a commit message to explain why this merge is necessary,

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -3,7 +3,7 @@ const data = {
     {
       name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
-      catagory: 'Shirts',
+      category: 'Shirts',
       image: '/images/p1.jpg',
       price: 120,
       countInStock: 10,
@@ -15,7 +15,7 @@ const data = {
     {
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
-      catagory: 'Shirts',
+      category: 'Shirts',
       image: '/images/p2.jpg',
       price: 250,
       countInStock: 20,
@@ -27,7 +27,7 @@ const data = {
     {
       name: 'Nike Slim Pants',
       slug: 'nike-slim-pants',
-      catagory: 'Pants',
+      category: 'Pants',
       image: '/images/p3.jpg',
       price: 25,
       countInStock: 15,
@@ -39,7 +39,7 @@ const data = {
     {
       name: 'Adidas Fit Pants',
       slug: 'adidas-fit-pants',
-      catagory: 'Pants',
+      category: 'Pants',
       image: '/images/p4.jpg',
       price: 65,
       countInStock: 5,
